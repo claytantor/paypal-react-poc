52200381c570a15e89212015d35d660077f85973
claytantor <claytantor@gmail.com>
2019-05-14 18:39:29 -0700

    adding example of failing import

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import moment from 'moment'; //imports fine
+
+//fails to import
+import paypal from '@paypal/checkout-components' 
+
+
+let PayPalButton = paypal.Buttons.driver('react', { React, ReactDOM });
+
+
+class PayPalButtonComponent extends React.Component {
+  createOrder(data, actions) {
+      return actions.order.create({
+          purchase_units: [{
+              amount: {
+                  value: '0.01'
+              }
+          }]
+      });
+  }
+  onApprove(data, actions) {
+      return actions.order.capture();
+  }
+  render() {
+      return (
+          <PayPalButton
+              createOrder={ (data, actions) => this.createOrder(data, actions) }
+              onApprove={ (data, actions) => this.onApprove(data, actions) }
+          />
+      );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Paypal POC</h1>
+        <div>This is where we do it.</div>
+        <div>
+          hello world {moment.utc().format("YYYY-MM-DD HH:MM:SS")}
+        </div>
       </header>
     </div>
   );
